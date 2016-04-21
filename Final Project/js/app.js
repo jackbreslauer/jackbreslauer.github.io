1dafbe853149b5e7114c84b28d2ef8583192ab0f
jackbreslauer <jabreslauer@gmail.com>
2016-04-20 22:55:32 -0400

    23

--- Final Project/js/app.js
+++ Final Project/js/app.js
@@ -1,3 +1,86 @@
+//var words = ['cat', 'dog', 'fish']
+//var i;
+//for (i = 0; i < substr.length; ++i) {
+    // do something with `substr[i]`
+//}
+
+
+var maincard=['a','b','c']
+var firstcard=['a','b','c']
+var secondcard=['a','b','c']
+var thirdcard=['a','b','c']
+var fourthcard=['a','b','c']
+
+// $('#next').click(function(){
+// 	$('#mainword').html(maincard[0])
+// 	$('#taboo1').html(firstcard[0])
+// 	$('#taboo2').html(secondcard[0])
+// 	$('#taboo3').html(thirdcard[0])
+// 	$('#taboo4').html(fourthcard[0])
+// })
+
+// $('#right').click(function(){
+// 	$('#mainword').html(maincard[1])
+// 	$('#taboo1').html(firstcard[1])
+// 	$('#taboo2').html(secondcard[1])
+// 	$('#taboo3').html(thirdcard[1])
+// 	$('#taboo4').html(fourthcard[1])
+// })
+
+var cards = [
+	{
+		word: 'cat',
+		hints: ['feline', 'kitty', 'meow','pussy']
+	},
+	{
+		word: 'dog',
+		hints: ['canine', 'puppy', 'woof','poochface']
+	},
+	{
+		word: 'car',
+		hints: ['auto', 'automobile', 'drive','vroom']
+	}
+]
+
+$('#right').click(function () {
+
+	// 1) get a random card
+	var randomNum = Math.floor(Math.random() * cards.length)
+	var randomCard = cards[randomNum]
+	// randomCard = {
+	// 	word: 'car',
+	// 	hints: ['auto', 'automobile', 'drive']
+	// }
+$('#mainword').html(randomCard.word)
+$('#taboo1').html(randomCard.hints[0])
+$('#taboo2').html(randomCard.hints[1])
+$('#taboo3').html(randomCard.hints[2])
+$('#taboo4').html(randomCard.hints[3])
+	// 2) print word to #mainword
+
+
+	// 3) print hints to #taboo1-4
+
+})
+
+//var card{
+//	main:[a,b,c,d,e]
+//	first:[f,g,h,i]
+//	second:[x,y,z]
+//}
+
+$('#down').click(function () {
+	maincard.forEach(cycle)
+})
+
+function cycle(item){
+	console.log(item)
+	$('#mainword').html(item)
+}
+//	// do something with word
+//})
+
+
 $(document).ready(function () {
 
 })
@@ -5,7 +88,8 @@ $(document).ready(function () {
 var score=0;
 
 $('#down').click(function(){
-	alert('pass')});
+//	alert('pass')
+});
 
 $('#left').click(function(){
 	score=score-1;
